e64aac8cefe3c3da99d3631af79ec447fd58638f
Cristian Emmanuel Silva <csilva@silvatech.org>
2020-01-10 05:59:23 -0600

    Using Angular Setup with embedded forms. Also edited app css

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
 import {Post} from '../post.model';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-post-create',
@@ -12,8 +13,11 @@ export class PostCreateComponent {
   enteredTitle = '';
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
-    const post: Post = {title: this.enteredTitle, content: this.enteredContent};
+  onAddPost(form: NgForm) {
+    if(form.invalid){
+      return;
+    }
+    const post: Post = {title: form.value.title, content: form.value.content};
     this.postCreated.emit(post);
   }
 }
