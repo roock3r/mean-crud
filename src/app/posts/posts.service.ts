8319068d1793072c80dba1b41fff705e3c8629ea
Cristian Emmanuel Silva <csilva@silvatech.org>
2020-01-17 09:36:39 -0600

    At Get Request and mapped the backend get with the front end model.

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 import {Post} from './post.model';
 
@@ -12,10 +13,19 @@ export class PostsService {
   constructor(private http: HttpClient){}
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((postData) => {
-        this.posts = postData.posts;
-        this.postsUpdated.next([...this.posts])
+    this.http.get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id:  post._id
+          };
+        });
+      }))
+      .subscribe(modifiedPosts => {
+        this.posts = modifiedPosts;
+        this.postsUpdated.next([...this.posts]);
       });
   }
 
