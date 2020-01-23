f34de669f9401b111c802b683600de90779e7f9f
Cristian Emmanuel Silva <csilva@silvatech.org>
2020-01-23 06:22:39 -0600

    Added image path using express and added styling.

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
@@ -17,6 +18,7 @@ mongoose.connect("mongodb+srv://dev02:hWKtC4XqedgdJehO@cluster0-ctv3y.mongodb.ne
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use("/images", express.static(path.join("backend/images")));
 
 app.use((req,res,next) => {
   res.setHeader('Access-Control-Allow-Origin', "*");
