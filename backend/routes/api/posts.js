7a61b24c2e8d07c48262b1986005d29af72f00f9
Cristian Emmanuel Silva <csilva@silvatech.org>
2020-01-23 15:03:20 -0600

    Added pagination functionality to backend.

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -70,7 +70,16 @@ router.put("/:id",multer({storage: storage}).single('image') ,(req, res, next) =
 });
 
 router.get('/', (req, res, next) => {
-  Post.find()
+  console.log(req.query);
+  const pageSize = +req.query.pagesize;
+  const currentPage = +req.query.page;
+  const postQuery = Post.find();
+  if(pageSize && currentPage){
+    postQuery
+    .skip(pageSize * currentPage - 1)
+    .limit(pageSize);
+  }
+  postQuery.find()
     .then(documents => {
       //console.log(documents);
       res.status(200).json({
