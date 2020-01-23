2617fe738e07fd9b2d64b9ff1019574d7a3dd74e
Cristian Emmanuel Silva <csilva@silvatech.org>
2020-01-22 19:23:24 -0600

    Added multer and configured storage on backend.

--- backend/routes/api/posts.js
+++ backend/routes/api/posts.js
@@ -1,9 +1,35 @@
 const express = require('express');
-const router = express.Router();
+const multer = require('multer');
+
 const mongoose = require('mongoose');
+
 const Post = require('../../models/post');
 
-router.post("/", (req, res) => {
+const router = express.Router();
+
+const MIME_TYPE_MAP = {
+  'img/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg',
+};
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error('Invalid mime type');
+    if (isValid) {
+      error = null;
+    }
+    cb(error, "backend/images");
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLocaleLowerCase().split(' ').join('-');
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + ext);
+  }
+});
+
+router.post("/", multer(storage).single('image'), (req, res) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content
@@ -18,18 +44,18 @@ router.post("/", (req, res) => {
 });
 
 router.put("/:id", (req, res, next) => {
-const post = new Post({
-  _id: req.body.id,
-  title: req.body.title,
-  content: req.body.content
-});
-  Post.updateOne({ _id: req.params.id }, post ).then(result => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({ _id: req.params.id }, post).then(result => {
     console.log(result);
-    res.status(200).json({message: 'Update Successful!'});
+    res.status(200).json({ message: 'Update Successful!' });
   })
 });
 
-router.get('/',(req, res, next) => {
+router.get('/', (req, res, next) => {
   Post.find()
     .then(documents => {
       //console.log(documents);
@@ -42,20 +68,20 @@ router.get('/',(req, res, next) => {
 
 router.get('/:id', (req, res, next) => {
   Post.findById(req.params.id).then(post => {
-    if(post){
+    if (post) {
       res.status(200).json(post);
-    }else{
-      res.status(404).json({message: 'post not found'});
+    } else {
+      res.status(404).json({ message: 'post not found' });
     }
   });
 });
 
 router.delete('/:id', (req, res, next) => {
   console.log(req.params.id);
-  Post.deleteOne({ _id : req.params.id}).then(result => {
-      console.log(result);
-      res.status(200).json({ message: 'post deleted!' })
-    });
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: 'post deleted!' })
+  });
 });
 
 module.exports = router;
