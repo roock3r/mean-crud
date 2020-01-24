bef475b383d5e9299c3c12cdcfc5756c1c90f944
Cristian Emmanuel Silva <csilva@silvatech.org>
2020-01-24 17:53:38 -0600

    added signup component 4 app with routes & module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,8 @@ import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
-import { LoginComponent } from './auth/login/login.component'
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -27,6 +28,7 @@ import { LoginComponent } from './auth/login/login.component'
     HeaderComponent,
     PostListComponent,
     LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
