78471fbc318695787e2bd973e078638e646a8e91
Cristian Emmanuel Silva <csilva@silvatech.org>
2020-01-24 18:46:31 -0600

    Added interface , http call, then sign up method.

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,20 @@
-import { Component } from "@angular/core";
+import { AuthService } from './../auth.service';
+import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-
 export class SignupComponent {
   isLoading = false;
 
-  onSignup(form: NgForm){
-    console.log(form);
+  constructor(public authService: AuthService) {}
+
+  onSignup(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.authService.createUser(form.value.email, form.value.password);
   }
 }
