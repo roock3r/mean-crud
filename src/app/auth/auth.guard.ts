2c9ba6b11424a1765d784f25c5aee5202faa6c1c
Cristian Emmanuel Silva <csilva@silvatech.org>
2020-03-19 06:33:47 -0600

    Implemented authGuard to check authentication.

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,21 @@
+import { AuthService } from './auth.service';
+import { CanActivate, ActivatedRouteSnapshot,RouterStateSnapshot, Router } from '@angular/router';
+import {Observable} from "rxjs";
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private authService:AuthService, private router: Router) {}
+
+  canActivate(
+    route: import("@angular/router").ActivatedRouteSnapshot,
+    state: import("@angular/router").RouterStateSnapshot
+    ): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
+      const isAuth = this.authService.getIsAuth();
+      if(!isAuth){
+        this.router.navigate(['/login']);
+      }
+      return isAuth;
+  }
+
+}
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { AuthData } from "./auth-data.model";
@@ -9,7 +10,7 @@ export class AuthService {
   private token: string;
   private authStatusListener = new Subject<boolean>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   getToken() {
     // return this.token;
@@ -44,6 +45,7 @@ export class AuthService {
       if (token) {
         this.isAuthenticated = true;
         this.authStatusListener.next(true);
+        this.router.navigate(['/']);
       }
       // this.token = token;
     });
@@ -53,5 +55,6 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
+    this.router.navigate(['/']);
   }
 }
