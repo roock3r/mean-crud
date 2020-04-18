331658ea73988e1da01242e5d8827dad96a819ec
Cristian Emmanuel Silva <csilva@silvatech.org>
2020-04-17 19:47:09 -0600

    added autthorization by user to crud of post .

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -41,7 +41,7 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return this.http.get<{ _id: string; title: string; content: string, imagePath: string }>(
+    return this.http.get<{ _id: string; title: string; content: string, imagePath: string, creator: string }>(
       `http://localhost:3000/api/posts/${id}`
     );
   }
@@ -75,7 +75,8 @@ export class PostsService {
       id: id,
       title: title,
       content: content,
-      imagePath: image
+      imagePath: image,
+      creator: null
       };
     }
     this.http
